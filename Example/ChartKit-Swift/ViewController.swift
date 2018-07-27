0a704607b235c3fcc9d6c66909631ed904cb8b1d
imjog <imjog24@gmail.com>
2018-07-27 14:18:42 +0530

    Added example project

---
+++ Example/ChartKit-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChartKit-Swift
+//
+//  Created by JOGENDRA on 27/07/18.
+//  Copyright © 2018 Jogendra Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
