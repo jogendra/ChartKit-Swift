1f5a90e4743bd8ebe3f3a561671c0318ebc65628
imjog <imjog24@gmail.com>
2018-07-28 17:31:08 +0530

    Added ClockChart and Contribution Graph classes

---
+++ Example/ChartKit-Swift/ChartKit/ClockChart.swift
@@ -0,0 +1,13 @@
+//
+//  ClockChart.swift
+//  ChartKit-Swift
+//
+//  Created by JOGENDRA on 28/07/18.
+//  Copyright © 2018 Jogendra Singh. All rights reserved.
+//
+
+import UIKit
+
+class ClockChart: UIView {
+
+}
---
+++ Example/ChartKit-Swift/ChartKit/ContributionGraph.swift
@@ -0,0 +1,13 @@
+//
+//  ContributionGraph.swift
+//  ChartKit-Swift
+//
+//  Created by JOGENDRA on 28/07/18.
+//  Copyright © 2018 Jogendra Singh. All rights reserved.
+//
+
+import UIKit
+
+class ContributionGraph: UIView {
+
+}
