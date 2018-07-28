afe535e42b1e08fe0815d638e9d53200604c8378
imjog <imjog24@gmail.com>
2018-07-28 17:24:08 +0530

    initialize BarChart

---
+++ Example/ChartKit-Swift/ChartKit/BarChart.swift
@@ -0,0 +1,13 @@
+//
+//  BarChart.swift
+//  ChartKit-Swift
+//
+//  Created by JOGENDRA on 28/07/18.
+//  Copyright © 2018 Jogendra Singh. All rights reserved.
+//
+
+import UIKit
+
+class BarChart: UIView {
+
+}
